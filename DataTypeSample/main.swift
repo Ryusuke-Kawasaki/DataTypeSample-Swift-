7b891d242d85b90916b5cbe5361f107b95802829
Ryusuke Kawasaki <kawasaki@codable.co.jp>
2017-09-05 15:47:00 +0900

    Initial Commit

---
+++ DataTypeSample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataTypeSample
+//
+//  Created by 川崎隆介 on 2017/09/05.
+//  Copyright © 2017年 codable. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
