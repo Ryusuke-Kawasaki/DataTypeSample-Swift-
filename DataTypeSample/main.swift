d4825dde3e820dc05f19b57e360e4af81d494234
Ryusuke Kawasaki <kawasaki@codable.co.jp>
2017-09-05 15:55:27 +0900

    Data型の確認

--- DataTypeSample/main.swift
+++ DataTypeSample/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+//バイナリデータから特定箇所のデータを抽出
+//target data
+let str = "abcde\nfghijk"
+//string to data
+var buffer:Data = str.data(using: .utf8)!
+//delimiter
+let delimstr = "\n"
+//string to data
+let delimData:Data = delimstr.data(using:.utf8)!
+
+//target dataのうちdelimiterまでの範囲を取得
+if let range = buffer.range(of: delimData) {
+    print(range)
+    //target dataからdelimiterまでのデータを抽出
+    let line = String(data: buffer.subdata(in: 0..<range.lowerBound), encoding: .utf8)
+    print(line!)
+    //target dataから抽出したデータを削除
+    buffer.removeSubrange(0..<range.upperBound)
+}
 
